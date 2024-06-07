34bbdde7c1828e045cd3d7c0f3cdff6bea797a58
marcosjunior00 <marcosantonio.jose01@gmail.com>
2024-06-06 22:51:59 -0300

    updating setup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const ChatRoutes = require('./src/routes/chat.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', ChatRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
