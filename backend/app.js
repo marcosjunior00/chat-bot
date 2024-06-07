3cbe76285e8bbc883fd5222828c61b8f1349b186
marcosjunior00 <marcosantonio.jose01@gmail.com>
2024-06-06 21:24:50 -0300

    feat: main file

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
