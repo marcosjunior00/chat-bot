01f3589553d1b22bb678615bc247a43ccd0d9945
marcosjunior00 <marcosantonio.jose01@gmail.com>
2024-06-26 20:04:12 -0300

    chore: icons

--- frontend/src/utils/iconGallery.jsx
+++ frontend/src/utils/iconGallery.jsx
@@ -1,8 +1,10 @@
-import { FaCircleUser, FaRobot } from 'react-icons/fa6';
+import { FaCircleUser, FaRobot, FaBroom, FaPaperPlane } from 'react-icons/fa6';
 
 const iconGallery = {
     user: (color, size, onClick) => <FaCircleUser fill={color} size={size} onClick={onClick} />,
     robot: (color, size, onClick) => <FaRobot fill={color} size={size} onClick={onClick} />,
+    broom: (color, size, onClick) => <FaBroom fill={color} size={size} onClick={onClick} />,
+    send: (color, size, onClick) => <FaPaperPlane fill={color} size={size} onClick={onClick} />,
 };
 
 export const renderIcon = ({ name, color, size, onClick }) => iconGallery[name](color, size, onClick);
